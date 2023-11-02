c13f639083b5c071f2a352801485d14d57e585d6
Michael Merry Okungbowa <michaelmerry8@gmail.com>
2023-11-02 09:45:39 +0000

    New commit

--- packages/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/config/config.py
@@ -11,7 +11,7 @@ pd.options.display.max_columns = 10
 
 PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
 TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
-DATASET_DIR = PACKAGE_ROOT / "datasets"
+DATASET_DIR = PACKAGE_ROOT / 'regression_model'/ "datasets"
 
 # data
 TESTING_DATA_FILE = "test.csv"
